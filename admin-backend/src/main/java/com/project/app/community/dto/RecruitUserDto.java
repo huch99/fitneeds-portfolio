03c30713d9b29241df60979e0a2c1ec622008bf2
gdbin1 <ot5037@naver.com>
2026-01-13 17:27:55 +0900

    01_13 dto 코드정리했던거 이상하게 원복된거 다시 정리

--- admin-backend/src/main/java/com/project/app/community/dto/RecruitUserDto.java
+++ admin-backend/src/main/java/com/project/app/community/dto/RecruitUserDto.java
@@ -1,23 +1,10 @@
 package com.project.app.community.dto;
 
+import lombok.Data;
+
+@Data
 public class RecruitUserDto {
 
     private Long userId;
     private String userName;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 }
