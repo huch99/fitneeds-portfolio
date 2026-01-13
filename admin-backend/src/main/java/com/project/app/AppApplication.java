7b13c54c21fb69e01b1bcf497e735385b428630e
youngnam81-kim <kimyn81@naver.com>
2026-01-13 12:34:33 +0900

    다중 커넥션 확인용.

--- admin-backend/src/main/java/com/project/app/AppApplication.java
+++ admin-backend/src/main/java/com/project/app/AppApplication.java
@@ -3,12 +3,16 @@ package com.project.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Spring Boot 애플리케이션의 시작점 (메인 클래스)
  */
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class AppApplication {
 
 	// 애플리케이션 실행 메소드
