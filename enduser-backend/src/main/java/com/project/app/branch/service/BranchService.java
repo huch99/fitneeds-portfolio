64a310977780c250cc96a6da83a9b6981648e46f
huch99 <huichan58@naver.com>
2026-01-22 12:08:06 +0900

    Feature/reservation/jhc - 정희찬 오류 수정 (#100)
    * Delete AdminTeachers.css
    
    * enum 문제로 스케줄 조회 안되는 문제 수정
    
    ScheduleSttsCd >> OPEN 추가
    서비스 및 검색 쿼리문 AVAILABLE 조회 =>>
    AVAILABLE , OPEN 모두 조회 로 변경
    
    * 오류 수정

--- enduser-backend/src/main/java/com/project/app/branch/service/BranchService.java
+++ enduser-backend/src/main/java/com/project/app/branch/service/BranchService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.app.branch.entity.Branch;
-import com.project.app.branch.repository.BranchRepository;
+import com.project.app.branch.respository.BranchRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class BranchService {
 
-    private final BranchRepository branchRepository;
+	private final BranchRepository branchRepository;
 
     public List<Branch> getAllBranches() {
         return branchRepository.findAll();
