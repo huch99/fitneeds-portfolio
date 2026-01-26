f341d049281a7c8c8b184265288d06fad4ab9654
huch99 <huichan58@naver.com>
2026-01-26 14:24:00 +0900

    보유 이용권 조회 오류 수정
    import 문제해결

--- enduser-backend/src/main/java/com/project/app/userpass/repository/UserPassRepository.java
+++ enduser-backend/src/main/java/com/project/app/userpass/repository/UserPassRepository.java
@@ -3,9 +3,10 @@ package com.project.app.userpass.repository;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.ibatis.annotations.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.project.app.userpass.entity.UserPass;
